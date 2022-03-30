bb7ea06834c8113b47ec0c61d55bceea7dfc051a
Haris <harismu@sheridancollege.ca>
2022-03-30 12:40:21 +0300

    classes sturcture created

--- Interactive_Quiz/Question.cs
+++ Interactive_Quiz/Question.cs
@@ -3,6 +3,14 @@ namespace Interactive_Quiz
 {
     public class Question
     {
+        public string QuestionText { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string CorrectAnswer { get; set; }
+
+
         public Question()
         {
         }
--- Interactive_Quiz/Quiz.cs
+++ Interactive_Quiz/Quiz.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Interactive_Quiz
 {
-    public class Quiz
+    public class Quiz : Question
     {
+        private List<Question> _questions;
+        private int _currentQuestionIndex;
+        public string Title { get; set; }
+        public int Score { get; private set; }
+
+
         public Quiz()
         {
+            _questions = new List<Question>();
         }
+
+        private void LoadQuestions()
+        {
+
+        }
+
+        private GetQuestionWithoutAnswer() : base()
+        {
+
+        }
+
+        public GetNextQuestion():base()
+        {
+
+        }
+
+        public bool CheckUserAnswer(string answer)
+        {
+            return true;
+        }
+
     }
 }
